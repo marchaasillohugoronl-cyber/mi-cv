c681d0e81f8d2666bfacc741cf813643234d010c
Hugo Ronal Machaca Sillo <hugoronaldev@gmail.com>
2026-02-13 00:02:01 -0500

    Preparar deploy a GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mi-cv/' // Debe coincidir con el nombre del repositorio en GitHub
+  base: '/mi-cv/'  // Debe coincidir con tu repo exacto
 })
